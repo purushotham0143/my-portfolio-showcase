248888c6ee30778c13bf2a1c2bb96667114e6334
Ragala Purushotham <ragalapurushotham@Ragalas-MacBook-Air.local>
2025-12-03 12:41:46 +0530

    route

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter ,Routes,Route} from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Gallery from "./components/portfolio/gallery"
+
 
 const queryClient = new QueryClient();
 
@@ -14,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
        <Route path="/" element={<Index />} />
 
@@ -24,7 +25,7 @@ const App = () => (
           {/* Catch All Route */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
